4aa036f1880458653bbcd75b1cf5b19e9038b0aa
姜明辉 <2996806099@qq.com>
2025-04-01 10:00:53 +0800

    feat: add agora-fls-sdk

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
-  <transition name="fade">
-    <router-view></router-view>
-  </transition>
+  <div>
+    声网集成试用
+    <div>
+      <video
+        id="player-container-id"
+        class="video-class"
+        preload="auto"
+        playsinline
+        webkit-playsinline
+        style="border-radius: 4px;"
+      ></video>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
+import {
+  LivePlayer,
+  PlayerEvent,
+  setRTCParameter,
+  enableLogUpload,
+  setLogLevel,
+  RtcEvent
+} from "agora-fls-sdk";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods:{
+    initAgoraPlayer() {
+
+    },
   }
+};
 </script>
